7f9d82929921cf09f197f471e1122249caa94145
zhk <hatuvu7180@gmail.com>
2025-07-09 15:31:34 +0700

    Randomize Tools Fix

--- Assets/Scripts/UI/UIRandomizeToolManager.cs
+++ Assets/Scripts/UI/UIRandomizeToolManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class UIRandomizeToolManager : MonoBehaviour
 {
@@ -30,20 +31,27 @@ public class UIRandomizeToolManager : MonoBehaviour
         toolIDS.Remove(matchToolID);
         selectedToolIDS.Add(matchToolID);
         
-        for(int i = 0; i < 2; i++) //random switch case
+        for(int i = 0; i < 2; i++) 
         {
             int randomToolID = toolIDS[Random.Range(0, toolIDS.Count)];
             toolIDS.Remove(randomToolID);
             selectedToolIDS.Add(randomToolID);
         }
 
-        for(int i = 0; i < 3; i++)
+        int randIndex = Random.Range(0, 2);
+        int a = selectedToolIDS[randIndex];
+        int b = selectedToolIDS[0];
+        selectedToolIDS[0] = a;
+        selectedToolIDS[randIndex] = b;
+
+        for (int i = 0; i < 3; i++)
         {
             toolUI[i].GetComponent<UIToolSelection>().SetNewTool(toolScriptableObjects[selectedToolIDS[i]]);
         }
 
         toolIDS.Clear();
         selectedToolIDS.Clear();
+
         for (int i = 0; i < toolScriptableObjects.Count; i++)
         {
             toolIDS.Add(i);
